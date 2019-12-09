ae426ea366cc49697d2071f697ae834a2e7bc1cc
Justin Scott <jrs@users.noreply.github.com>
2019-12-08 17:10:35 -0800

    Update Player.cs

--- course-units/unit-3-first-2D-game/galaxy-space-shooter/Player.cs
+++ course-units/unit-3-first-2D-game/galaxy-space-shooter/Player.cs
@@ -1,26 +1,55 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
+    private float _speedModifier = 2f;
     [SerializeField] private GameObject _laserPrefab;
-    [SerializeField] private float _fireRate = 0.5f;
-    [SerializeField] private float _canFire = -1.0f;
+    [SerializeField] private GameObject _tripleShotPrefab;
+    private Vector3 _laserOffset = new Vector3(0, 1.05f, 0);
+    private float _fireRate = 0.5f;
+    private float _canFire = -1.0f;
     [SerializeField] private int _lives = 3;
-    [SerializeField] private Vector3 _laserOffset = new Vector3(0, 1.0f, 0);
-    [SerializeField] private bool _isShieldPowerupActive = false;
-    [SerializeField] private bool _isSpeedPowerupActive = false;
-    [SerializeField] private bool _isTripleshotActive = false;
-    [SerializeField] private float _speedModifier = 2f;
-
     private SpawnManager _spawnManager;
+    [SerializeField] private bool _isTripleShotActive = false;
+    [SerializeField] private bool _isSpeedBoostActive = false;
+    [SerializeField] private bool _isShieldsActive = false;
+    [SerializeField] private GameObject _shieldVisualizer;
+    [SerializeField] private GameObject _leftEngine;
+    [SerializeField] private GameObject _rightEngine;
+    [SerializeField] private int _score;
+    private UIManager _uiManager;
+    [SerializeField] private AudioClip _laserAudio;
+    private AudioSource _playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
+        //take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
-        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        _playerAudio = GetComponent<AudioSource>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is null.");
+        }
+
+        if(_uiManager == null)
+        {
+            Debug.LogError("The UI Manager is null.");
+        }
+
+        if(_playerAudio == null)
+        {
+            Debug.LogError("Player Audio is NULL");
+        }
+
+        _playerAudio.clip = _laserAudio;
+        
     }
 
     // Update is called once per frame
@@ -36,46 +65,97 @@ public class Player : MonoBehaviour
 
     void CalculateMovement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        //local variables only accessible in the Update()
         float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
+        /*
+        Vector3.right == new Vector3(1,0,0);
+        transform.Translate(Vector3.right * horizontalInput *_speed * Time.deltaTime);
+        Vector3.up == new Vector3(0,1,0);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        below is a short hand of the 2 above lines of code
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+        */
+
+        //optimized code for above content
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
         transform.Translate(direction * _speed * Time.deltaTime);
         
-        //keep the player bound to a set area on the Y axis
-        if(transform.position.y >= 0)
+        /*
+        //keeps the player bound to a set area on the Y axis
+        if (transform.position.y >= 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
-        else if(transform.position.y <= -3.5f)
+        else if (transform.position.y <= -3.8)
         {
-            transform.position = new Vector3(transform.position.x, -3.5f, 0);
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
+        */
+        //optimized code from above content
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,-3.5f, 0), 0);
 
         //keep the player bound to a set area on the X axis
-        if (transform.position.x >= 9f)
+        if (transform.position.x >= 11.3f)
         {
-            transform.position = new Vector3(9f, transform.position.y, 0);
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        else if (transform.position.x <= -9f)
+        else if (transform.position.x <= -11.3f)
         {
-            transform.position = new Vector3(-9f, transform.position.y, 0);
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
     }
 
     void FireLaser()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity); 
+
+        if(_isTripleShotActive == true)
+        {
+            Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity);
+        }
+
+        _playerAudio.Play();
+        
     }
 
     public void Damage()
     {
-        //if player is hit by enemy -1 from lives
-        _lives--; //_lives = _lives - 1;
+        //if shields is active
+        //do nothing...
+        //deactivate shields
+        //return;
+        if(_isShieldsActive == true)
+        {
+            _isShieldsActive = false;
+            //disable the visualizer
+            _shieldVisualizer.SetActive(false);
+            return;
+        }
 
-        //if player has 0 lives, destroy the player
-        if (_lives < 1)
+        _lives--;
+
+        _uiManager.UpdateLives(_lives);
+
+        //turn on damage
+        if(_lives == 2)
+        {
+            _leftEngine.SetActive(true);
+        }
+        else if(_lives == 1)
+        {
+            _rightEngine.SetActive(true);
+        }
+
+        //check if dead
+        //destroy us
+        if(_lives < 1)
         {
             _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
@@ -84,44 +164,50 @@ public class Player : MonoBehaviour
 
     public void TripleShotActive()
     {
-        //when player collides with powerup object
-        _isTripleshotActive = true;
-
-        //start the power down coroutine for tripleshot
+        //tripleShotAcitve becomes true
+        //start the power down coroutine for triple shot
+        _isTripleShotActive = true;
         StartCoroutine(TripleShotPowerDownRoutine());
     }
 
+    //IEnumerator TripleShotPowerDownRountine
+    //wait 5 seconds
+    //set the triple shot to false
     IEnumerator TripleShotPowerDownRoutine()
     {
-        //wait 5 seconds, then turn off triple shot
-        yield return new waitForSecounds(5.0f);
-        _isTripleshotActive = false;
+        yield return new WaitForSeconds(5.0f);
+        _isTripleShotActive = false;
     }
 
     public void SpeedBoostActive()
     {
-        //when player collides with powerup object
-        _isSpeedPowerupActive = true;
-        _speed *= _spawnManager;
-
-        //start the power down coroutine for speed boost
+        //speedBoostActive becomes true
+        //start the speed cool down coroutine for speed boost
+        _isSpeedBoostActive = true;
+        _speed *= _speedModifier;
         StartCoroutine(SpeedBoostPowerDownRoutine());
     }
 
     IEnumerator SpeedBoostPowerDownRoutine()
     {
-        //wait 5 sconds, then turn off speed boost
-        yield return new waitForSecounds(5.0f);
-        _isSpeedPowerupActive = false;
+        yield return new WaitForSeconds(5.0f);
+        _isSpeedBoostActive = false;
         _speed /= _speedModifier;
     }
 
     public void ShieldsActive()
     {
-        //when player collides with powerup object
         _isShieldsActive = true;
-
-        //turn on the visual look of the sheid
-        //code goes here
+        //enable visualize
+        _shieldVisualizer.SetActive(true);
     }
+    
+    //method to add points to the score
+    //communicate with the UI to update the score
+    public void AddScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdateScore(_score);
+    }
+
 }
