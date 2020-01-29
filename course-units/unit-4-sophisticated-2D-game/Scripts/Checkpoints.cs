c8fddbdb10ab26dfc472fce0015244d9e176a89a
Justin Scott <jrs@users.noreply.github.com>
2020-01-29 09:22:31 -0800

    Add files via upload

---
+++ course-units/unit-4-sophisticated-2D-game/Scripts/Checkpoints.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    [Header("Status Flags")]
+    public bool hasBeenChecked = false;
+
+    private Animator _anim;
+    private float waitTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasBeenChecked == true)
+        {
+            //set the checkpoint on flag in the animator
+            _anim.SetBool("hasBeenChecked", hasBeenChecked);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && !hasBeenChecked)
+        {
+            hasBeenChecked = true;
+            GameManger.instance.SetSpawnPoint(transform.position);
+            
+        }
+    }
+}
---
+++ course-units/unit-4-sophisticated-2D-game/Scripts/Collectibles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public bool isItem, isHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(isItem)
+        {
+            //Debug.Log("You collided with your collectible.");
+            GameManger.instance.AddToItemCount();
+            Destroy(this.gameObject);
+        }
+    }
+}
